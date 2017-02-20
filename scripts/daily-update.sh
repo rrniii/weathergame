4aca909727835492cb4d3ae1264fa00e828a810b
eardkdw <eardkdw@ncas-field2>
2017-02-20 17:04:00 +0000

    Switch to tower measurements instead of Davis windspeeds

--- scripts/daily-update.sh
+++ scripts/daily-update.sh
@@ -1,12 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 #DAVISDBFILE=/home/shareddata/met/dbfile/wview-archive.sdb
-DAVISDBFILE=/data/shareddata/met/sql/wview-backup-`date --utc +%A`.sdb
+DAVISDBFILE=/var/www/html/weather/sql/weewx.sdb
 
+DAY=${1:-'yesterday'}
 
 #Only works correctly after 1800GMT.
-START=`date +%Y-%m-%d -d 'yesterday'`
-END=`date +%Y-%m-%d `
+START=`date +%Y-%m-%d -d "${DAY}"`
+END=`date +%Y-%m-%d -d "${DAY} +1 days"`
 
-SQL1="SELECT ROUND(Min((OutTemp - 32.0) * (5.0/9.0)),1) AS MinTemp, ROUND(MAX((OutTemp - 32.0) * (5.0/9.0)),1) AS MaxTemp, CAST(ROUND(SUM((rain))*25.4) AS integer) AS rainfall FROM archive WHERE DATETIME(dateTime, 'unixepoch') > '${START} 17:59:59' AND DATETIME(dateTime, 'unixepoch') < '${END} 18:00:00';"
-SQL2="SELECT CAST(ROUND(windDir,0) AS integer), CAST(ROUND(windSpeed * 0.447027) AS integer) FROM archive WHERE DATETIME(dateTime,'unixepoch') = '${END} 12:00:00';"
-sqlite3 $DAVISDBFILE "${SQL1}${SQL2}" | tr '\n' '|' | awk -F '|' '{print "INSERT INTO forecasts (day, group_id, min_temp, max_temp, total_rainfall, wind_direction, wind_speed) VALUES (\"'${START}'\", 10, "$1", "$2", "$3", "$4", "$5");"}' | mysql -umeasure -p4measure measurements
+
+SQL1="SELECT ROUND(Min((OutTemp - 32.0) * (5.0/9.0)),1) AS MinTemp, ROUND(MAX((OutTemp - 32.0) * (5.0/9.0)),1) AS MaxTemp, CAST(ROUND(SUM((rain))*25.4) AS integer) AS rainfall FROM archive WHERE DATETIME(dateTime, 'unixepoch') > '${START} 18:59:59' AND DATETIME(dateTime, 'unixepoch') < '${END} 18:00:00';"
+
+#SQL2="SELECT CAST(ROUND(windDir,0) AS integer), CAST(ROUND(windSpeed * 0.447027) AS integer) FROM archive WHERE DATETIME(dateTime,'unixepoch') = '${END} 12:00:00';"
+PROCTOWERFILE="/data/shareddata/tower/Proc/001w/wind-temp-rh-001-${END}.csv"
+WIND=( $(cat $PROCTOWERFILE | grep "$END 12:00:00" | awk -F ',' '{print "/var/www/html/measurements/scripts/polar.py " $2" " $3}' | bash) )
+sqlite3 $DAVISDBFILE "${SQL1}" | tr '\n' '|' | awk -F '|' '{print "INSERT INTO forecasts (day, group_id, min_temp, max_temp, total_rainfall, wind_direction, wind_speed) VALUES (\"'${START}'\", 10, "$1", "$2", "$3", '${WIND[1]}', '${WIND[0]}' );"}' | mysql -umeasure -p4measure measurements
